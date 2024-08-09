f19b6ed7c16436d811df7898cb41c417f135ac07
Harigovind.V <harigovind775@gmail.com>
2024-08-09 12:06:22 +0530

    finished

---
+++ src/Components/Add.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+      <div className="container">
+      <nav class="navbar fixed-top navbar-expand-lg bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ToDo App</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Add</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/search">Search</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/view">View</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        <div className="row g-3" style={{marginTop:'50px'}}>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">User-ID</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">ID</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">TITLE</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">Completed Status</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success">Submit</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Add
